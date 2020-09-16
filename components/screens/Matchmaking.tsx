d06ab5851a98c9bcdd3b739b26cb71e9977e7555
JJ <justinjcode@gmail.com>
2020-09-16 15:24:09 -0700

    add button

--- components/screens/Matchmaking.tsx
+++ components/screens/Matchmaking.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button } from 'react-native'
 
 import firestore from '@react-native-firebase/firestore'
 
