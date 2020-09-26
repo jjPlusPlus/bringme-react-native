0390baf331c65b4033816318b7a579dca613f9b9
JJ <justinjcode@gmail.com>
2020-09-26 16:01:50 -0700

    the Match screen is now more of a logical container component

--- components/screens/Match.tsx
+++ components/screens/Match.tsx
@@ -1,47 +1,87 @@
-import React, { useRef } from 'react'
-import { RNCamera } from 'react-native-camera'
-
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-export default function Match() {
-  // let camera = useRef<RNCamera | null>(null)
-  return (
-    <View style={styles.container}>
-      {/* <RNCamera
-        ref={ref => {
-          camera = ref;
-        }}
-        style={styles.preview}
-        type={RNCamera.Constants.Type.back}
-        flashMode={RNCamera.Constants.FlashMode.on}
-        androidCameraPermissionOptions={{
-          title: 'Permission to use camera',
-          message: 'We need your permission to use your camera',
-          buttonPositive: 'Ok',
-          buttonNegative: 'Cancel',
-        }}
-        androidRecordAudioPermissionOptions={{
-          title: 'Permission to use audio recording',
-          message: 'We need your permission to use your audio',
-          buttonPositive: 'Ok',
-          buttonNegative: 'Cancel',
-        }}
-      /> */}
-    </View>
+import firestore from '@react-native-firebase/firestore'
+
+import MatchHostView from '../MatchHostView'
+import MatchPlayerView from '../MatchPlayerView'
+
+/* To Do: 
+ * On load, Check if the user is a Host or Player, kick user if they are not either one
+ * Then, show the user the MatchPlayerView or the MatchHostView
+ * 
+ * Either: 
+ *  - declare all actions on the Match object here, or
+ *  - pass down the Match ref() to the Views
+ * 
+ * Actions that can happen:
+ *   setRoundWord
+ *   startRound
+ *   submitWord
+ *   endMatchEarly
+ *   leaveMatch (player quits match)
+ *   (future) voteToSkip
+*/ 
+
+export default function Match(props) {
+  const matchId = props.route.params.matchId
+
+  const [user, setUser] = useState(null)
+  const [match, setMatch] = useState(null)
+
+  // get user
+  useEffect(() => {
+    // get the full current user document
+    firestore()
+      .collection('users')
+      .where('user', '==', props.user.uid)
+      .get()
+      .then(querySnapshot => {
+        if (!querySnapshot) {
+          return console.error('users query failed')
+        }
+        const data = querySnapshot.docs[0].data()
+        const withId = {
+          ...data,
+          id: querySnapshot.docs[0].id
+        }
+        return setUser(withId)
+      })
+    firestore()
+      .collection('matches')
+      .doc(matchId)
+      .onSnapshot(documentSnapshot => {
+        if (!documentSnapshot) {
+          return
+        }
+        const data = documentSnapshot.data()
+        const withId = {
+          ...data,
+          id: documentSnapshot.id
+        }
+        setMatch(withId)
+      })
+  }, [])
+
+  const setRoundWord = () => {
+
+  }
+
+  const startRound = () => {
+
+  }
+  
+  const submitWord = () => {
+
+  }
+
+  if (!user) {
+    return <View><Text>Loading</Text></View>
+  }
+
+  return user.id === match.host.uid ? (
+    <MatchHostView match={match} setRoundWord={setRoundWord} startRound={startRound}/>
+  ) : (
+    <MatchPlayerView match={match} submitWord={submitWord} />
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-  },
-  preview: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-})
