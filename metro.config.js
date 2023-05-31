9759e69d29fd87f58b314fbce7ad8f9a3ca0745b
JJ Medina <justinjcode@gmail.com>
2023-05-30 18:35:34 -0700

    bugfixes

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,10 @@
+
 module.exports = {
   transformer: {
     assetPlugins: ['expo-asset/tools/hashAssetFiles'],
   },
   resolver: {
-   sourceExts: ['jsx','js','ts','tsx'] //add here 
+    sourceExts: ['jsx','js','ts','tsx', 'ios.js', 'android.js'] //add here 
   },
-};
+}
+
