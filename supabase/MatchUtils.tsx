644cb5e1b6658ec7582a5b44ce933217e5ae8a94
JJ Medina <justinjcode@gmail.com>
2024-03-01 16:58:00 -0800

    round start/end utilities + new edge function

--- supabase/MatchUtils.tsx
+++ supabase/MatchUtils.tsx
@@ -86,6 +86,15 @@ export function useMatchData(room_code:string | undefined) {
             filter: `match_id=eq.${matchId}` 
           }, getMatchData
         )
+        .on(
+          'postgres_changes',
+          {
+            event: '*',
+            schema: 'public',
+            table: 'rounds',
+            filter: `match_id=eq.${matchId}`
+          }, getMatchData
+        )
         .subscribe((status, err) => {
           if (status) {
           } else if (err) {
@@ -121,10 +130,77 @@ export function useMatchData(room_code:string | undefined) {
         match_id: matchData?.id
       },
     })
-
     if (error) {
       return console.log('error starting match: ', error)
     }
+  }
+
+  const startRound = async (round: Round, roundWord: string) => {
+    if (!roundWord || roundWord.length < 1) {
+      return
+    }
+    console.log('starting round')
+    const { data, error } = await supabase.functions.invoke('start-round', {
+      body: { 
+        round: round,
+        roundWord: roundWord
+      },
+    })
+    if (error) {
+      return console.log('error starting round: ', error)
+    }
+    
+  }
+
+  const getNextLeader = () => {
+    const { players, rounds } = matchData
+    const currentRound = rounds.find((r:Round) => r.round_index === matchData.round_index) || rounds[0]
+    const currentLeader = players.find((p:User) => p.id === currentRound.leader)
+    const currentLeaderIndex = players.indexOf(currentLeader)
+    const nextLeaderIndex = currentLeaderIndex + 1
+    const nextLeader = players[nextLeaderIndex].id
+    return nextLeader
+  }
+
+  const endRound = async (round: Round, player: User) => {
+    const time_remaining = round.time - (Math.round( Date.now() / 1000 ) - Math.round( new Date(round.started_at).getTime() / 1000 ))
+    const final_score = 100 + time_remaining
+    // update the current round
+    const { data: roundUpdateData, error: roundUpdateError } = await supabase
+      .from('rounds')
+      .update({ 
+        status: 'COMPLETE',
+        finished_at: new Date().toISOString(),
+        time_remaining: time_remaining,
+        winner: player.id,
+        points: final_score
+      })
+      .eq('id', round.id)
+
+    // get the next round
+    const { data: next_round, error: next_round_error } = await supabase
+      .from('rounds')
+      .select('*')
+      .eq('match_id', matchData.id)
+      .eq('round_index', round.round_index + 1)
+      .single()
+
+      // prepare the next round
+    const { data: nextRoundData, error: nextRoundError } = await supabase
+      .from('rounds')
+      .update({
+        status: 'STARTED',
+        leader: getNextLeader()
+      })
+      .eq('id', next_round.id)
+
+    // update the match
+    const { data: matchUpdateDate, error: matchUpdateError } = await supabase
+      .from('matches')
+      .update({ 
+        round_index: matchData.round_index + 1
+      })
+      .eq('id', matchData.id)
   }
 
   const [matchData, setMatchData] = useState<any>()
@@ -142,6 +218,8 @@ export function useMatchData(room_code:string | undefined) {
   return [
     matchData,
     startMatch,
-    leaveMatch
+    leaveMatch,
+    startRound,
+    endRound
   ]
 }
---
+++ supabase/functions/start-round/index.ts
@@ -0,0 +1,109 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
+const supabase_client = createClient(
+  // Supabase API URL - env var exported by default when deployed.
+  Deno.env.get('SUPABASE_URL') ?? '',
+  // Supabase API SERVICE ROLE KEY - env var exported by default when deployed.
+  Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? ''
+)
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+}
+Deno.serve(async (req: any) => {
+  try {
+    const { round, roundWord } = await req.json() 
+    if (!round) throw new Error("Missing 'round' in request body")
+
+    const { data, error } = await supabase_client
+      .from('rounds')
+      .update({ 
+        status: 'IN_PROGRESS',
+        word: roundWord,
+        started_at: new Date().toISOString(),
+      })
+      .eq('id', round.id)
+
+    const getNextLeader = (match_snapshot: any) => {
+      const { players, rounds } = match_snapshot
+      const currentRound = rounds.find((r:any) => r.round_index === match_snapshot.round_index) || rounds[0]
+      const currentLeader = players.find((p:any) => p.id === currentRound.leader)
+      const currentLeaderIndex = players.indexOf(currentLeader)
+      const nextLeaderIndex = currentLeaderIndex + 1
+      const nextLeader = players[nextLeaderIndex].id
+      return nextLeader
+    }
+    
+    // using a javascript timeout feels gross to me... I would prefer to use a background worker or something
+    // but I don't know how to do that in deno/supabase yet. 
+    // This timeout could be unreliable if the server is under heavy load.
+    setTimeout(async () => {
+      const { data: round_snapshot, error: round_snapshot_error } = await supabase_client
+        .from('rounds')
+        .select('*')
+        .eq('id', round.id)
+        .single()
+
+      if (round_snapshot?.status !== 'COMPLETE') {
+
+        const { data: match_snapshot, error: match_snapshot_error } = await supabase_client
+          .from('matches')
+          .select(`
+            id,
+            room_code,
+            status,
+            round_index,
+            rounds ( id, round_index, status, leader ),
+            players:users!players ( id, username )
+          `)
+          .eq('id', round_snapshot.match_id)
+          .single()
+        
+        // Time out the current round
+        // Todo: this could potentially race against a current end-game call in a close game
+        // we need to decide to keep everything on the server side or not
+        await supabase_client
+          .from('rounds')
+          .update({ 
+            status: 'TIMED_OUT',
+            finished_at: new Date().toISOString(),
+          })
+          .eq('id', round.id)
+
+        // prepare the next round
+        const next_leader = getNextLeader(match_snapshot)
+        console.log(next_leader || 'oh well?')
+        await supabase_client
+          .from('rounds')
+          .update({
+            status: 'STARTED',
+            leader: next_leader
+          })
+          .eq('match_id', round_snapshot.match_id)
+          .eq('round_index', round_snapshot.round_index + 1)
+        
+        // update the match
+        await supabase_client
+          .from('matches')
+          .update({ 
+            round_index: match_snapshot.round_index + 1
+          })
+          .eq('id', match_snapshot.id)
+      }
+    }, 60000 ) // 60 seconds
+
+    return new Response(
+      JSON.stringify({ data, error }),
+      { 
+        headers: { "Content-Type": "application/json" },
+        status: 200 
+      },
+    )
+  } catch (error: any) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: 400,
+    })
+  }
+})
+
