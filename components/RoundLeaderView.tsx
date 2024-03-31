0d50f352d97d7e1aa3eb8815148dcd18f00f1a81
JJ Medina <justinjcode@gmail.com>
2024-03-30 17:49:48 -0700

    re-organized leader view

--- components/RoundLeaderView.tsx
+++ components/RoundLeaderView.tsx
@@ -5,6 +5,7 @@ import { TouchableOpacity } from 'react-native-gesture-handler'
 
 import useTimeRemaining from '../utils/useTimeRemaining'
 import AnnouncementHeader from './AnnouncementHeader'
+import { ROUND_STATES } from './constants'
 
 const divider = require('../assets/divider.png')
 const loading = require('../assets/loading.png')
@@ -25,53 +26,108 @@ const RoundLeaderView: FunctionComponent<RoundLeaderViewProps> = (props) => {
   const { user, round, players, room_code, startRound, submissions, acceptSubmission } = props
   const [roundWord, setRoundWord] = useState<string>('')
 
+  switch (round.status) {
+    case ROUND_STATES.STARTING:
+      // Countdown to round start
+      return <RoundStarting round={round} />
+    case ROUND_STATES.IN_PROGRESS:
+      // Player is taking & submitting their picture
+      return <RoundInProgress round={round} user={user} players={players} submissions={submissions} acceptSubmission={acceptSubmission}/>
+    case ROUND_STATES.ACTIVE:
+      // Round leader is choosing a word
+      return <RoundActive round={round} roundWord={roundWord} setRoundWord={setRoundWord} startRound={startRound} />
+    default:
+      // Fallback in case we end up in an unexpected state
+      return <FallbackState />
+  }
+}
+
+/* Counting down */
+const RoundStarting: FunctionComponent<{round: Round}> = (props) => {
+  const { round } = props
   return (
     <View className="bg-white flex h-full p-4">
       <AnnouncementHeader>
         <View>
           <Text className="font-lucky text-3xl text-bmBlue uppercase">
-            {round.status === 'IN_PROGRESS' ? `Bring me...` : `You're the king`}
+            Round starting
           </Text>
         </View>
       </AnnouncementHeader>
-      {round.status === 'IN_PROGRESS' ? (
-        <>
-          <View>
-            <Image source={divider} />
-            <Text className="font-lucky text-4xl mb-4 mt-5 text-center uppercase">{round.word}</Text>
-            <Image className="" source={divider} />
-          </View>
-          <MaterialIcons name="timer" size={24} color="black" />
-          <RoundPlayers players={players} submissions={submissions} acceptSubmission={acceptSubmission} round={round} user={user} />
-        </>
-      ) : (
-        <View className="my-4">
-          <Text className="font-medium text-base">What do you want the others to bring to you?</Text>
-          <StyledInput
-            placeholder=""
-            onChangeText={(text) => {
-              // handle text input change
-              setRoundWord(text)
-            }}
-            value={roundWord}
-          />
-          <StyledButton
-            onPress={() => startRound(round, roundWord)}
-          >
-            <StyledButtonText>Start Round</StyledButtonText>
-          </StyledButton>
-        </View>
-      )}
+      <RoundTimer round={round} />
     </View>
   )
 }
 
-const RoundTimer = (props: any) => {
+/* Accepting submissions */
+const RoundInProgress: FunctionComponent<{round: Round, user: User, submissions: Submission[], players: User[], acceptSubmission: any}> = (props) => {
+  const { round, user, players, submissions, acceptSubmission } = props
+  return (
+    <View className="bg-white flex h-full p-4">
+      <AnnouncementHeader>
+        <View>
+          <Text className="font-lucky text-3xl text-bmBlue uppercase">
+            Bring me...
+          </Text>
+        </View>
+      </AnnouncementHeader>
+      <>
+        <View>
+          <Image source={divider} />
+          <Text className="font-lucky text-4xl mb-4 mt-5 text-center uppercase">{round.word}</Text>
+          <Image source={divider} />
+        </View>
+        <RoundTimer round={round} />
+        <RoundPlayers players={players} submissions={submissions} acceptSubmission={acceptSubmission} round={round} user={user} />
+      </>
+    </View>
+  )
+}
+
+/* Choosing a word */
+const RoundActive: FunctionComponent<{round: Round, setRoundWord: any, roundWord: string, startRound: any}> = (props) => {
+  const { round, setRoundWord, roundWord, startRound } = props
+  return (
+    <View className="bg-white flex h-full p-4">
+      <AnnouncementHeader>
+        <View>
+          <Text className="font-lucky text-3xl text-bmBlue uppercase">
+            You're the king
+          </Text>
+        </View>
+      </AnnouncementHeader>
+      <View className="my-4">
+        <Text className="font-medium text-base">What do you want the others to bring to you?</Text>
+        <StyledInput
+          placeholder=""
+          onChangeText={(text) => {
+            // handle text input change
+            setRoundWord(text)
+          }}
+          value={roundWord}
+        />
+        <StyledButton
+          onPress={() => startRound(round, roundWord)}
+        >
+          <StyledButtonText>Start Round</StyledButtonText>
+        </StyledButton>
+      </View>
+    </View>
+  )
+}
+
+const FallbackState = () => {
+  return (
+    <View><Text>eh?</Text></View>
+  )
+}
+
+const RoundTimer: FunctionComponent<{round:Round}> = (props) => {
   const { round } = props
   const remaining_time = useTimeRemaining(round.started_at, round.time)
   return (
     <View>
-      <Text>Time Remaining: {remaining_time}</Text>
+      <Text className="font-luck items-center text-5xl">{remaining_time || ''}</Text>
     </View>
   )
 }
